d5df9f090ce8f3436831fe1429dd71def481ba94
Carlos Lopez-Camey <c.lopez@kmels.net>
2011-08-02 03:07:27 -0600

    adds put_pixel(); draws line with bresenhams algorithm; brush is now done with new line algorithm

--- src/tools.c
+++ src/tools.c
@@ -1,20 +1,71 @@
 #include <gtk/gtk.h>
 #include "global.h"
+//#include <cstdlib>
+#include <stdlib.h>
+
+/* paints a single pixel in coordinate x,y*/
+inline void put_pixel(cairo_t *cr,double x, double y){
+  cairo_move_to(cr,x,y);
+  cairo_rectangle(cr,x,y,1,1);
+  cairo_fill(cr);
+}
+
+/* Line, draws a line from x1,y1 to coordinate x2,y2 using Bresenham's algorithm*/
+inline void line(cairo_t *cr, double x1, double y1, double x2, double y2){
+  gboolean steep = FALSE;
+  double tmp;
+  if (abs(y2-y1) > abs(x2 - x1)){
+    steep = TRUE;
+    //swap x1, y1
+    tmp = y1;
+    y1 = x1;
+    x1 = tmp;
+    //swap x2,y2
+    tmp = y2;
+    y2 = x2;
+    x2 = tmp;
+  }
+  
+  if (x1 > x2){
+    //swap x1, x2
+    tmp = x2;
+    x2 = x1;
+    x1 = tmp;
+    //swap y1,y2
+    tmp = y2;
+    y2 = y1;
+    y1 = tmp;
+  }
+
+  int dx = x2 - x1;
+  int dy = abs(y2 - y1);
+   
+  double discriminant = 2*dy - 2*dx;
+  double next_y = y1;
+
+  int ystep = 1;
+  if (y1 > y2)
+    ystep = -1;
+
+  int x;
+  for(x = x1; x <= x2 ; x ++){
+    if (steep)
+      put_pixel(cr,next_y,x);
+    else
+      put_pixel(cr,x,next_y);
+    
+    if (discriminant <= 0)
+      discriminant = discriminant + 2*dy;
+    else{
+      next_y = next_y + ystep;
+      discriminant = discriminant + 2*dy - 2*dx;
+    }
+  }
+}
 
 /* Brush, paints a single pixel in coordinate x,y*/
 inline void brush(cairo_t *cr, double x1, double y1, double x2, double y2){
-  cairo_move_to(cr, x1, y1);
-  cairo_line_to(cr, x2, y2);	
-  cairo_stroke(cr);
-  //printf("pattern: %p\n",cairo_get_source(cr));
-}
-
-/* Line, draws a line from x1,y1 to coordinate x2,y2*/
-inline void line(cairo_t *cr, double x1, double y1, double x2, double y2){
-  //printf("%f,%f->%f,%f\n",x1,y1,x2,y2);
-  cairo_move_to(cr, x1, y1);
-  cairo_line_to(cr, x2, y2);	
-  cairo_stroke(cr);
+  line(cr,x1,y1,x2,y2);
 }
 
 /* Saves a surface, tipically called before drawing anything new, so we can simulate drawing on it*/
